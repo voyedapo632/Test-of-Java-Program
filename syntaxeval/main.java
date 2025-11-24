3d566ec875185849f8957f9e1cdb72c59b28bb3a
voyedapo632 <voyedapo632@lsr7.net>
2025-11-24 11:59:35 -0600

    Something

---
+++ syntaxeval/main.java
@@ -0,0 +1,29 @@
+package syntaxeval;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String loadFile(String path) {
+        String text = "";
+        File file = new File(path);
+        Scanner sn;
+
+        try {
+            sn = new Scanner(file);
+
+            while (sn.hasNextLine()) {
+                text += sn.nextLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        return text;
+    }
+}
